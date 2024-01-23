06b65f028532413268df6f0d3d6568c7b1b304a5
Ahmed Alkaf <ahmedalkaff@yahoo.com>
2024-01-23 14:07:49 +0300

    next config to function

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
+const nextConfig = () => {
+  return {
+    output: 'export',
+  }
 }
 
 module.exports = nextConfig
